b508d66e6409c6dbd2df3b23cce125d2fd12c7ab
Tobias Müller <metvernichter.github@gmail.com>
2019-05-22 21:21:38 +0200

    working 1.0

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ class AvatarCollector(ts3plugin):
     version         = "1.0"
     apiVersion      = 21
     author          = "Luemmel"
-    description     = "Cleans your contact list."
+    description     = "Delete contacts which you haven't seen for the selected time."
     offersConfigure = True
     commandKeyword  = ""
     infoTitle       = None
@@ -44,7 +44,7 @@ class Dialog(QDialog):
     def __init__(self, cc, parent=None):      
         self.cc = cc
         super(QDialog, self).__init__(parent)       
-        #self.setWindowIcon(QIcon(os.path.join(getPluginPath(), "pyTSon", "scripts", "contactcleaner", "icon.png")))
+        self.setWindowIcon(QIcon(os.path.join(getPluginPath(), "pyTSon", "scripts", "contactcleaner", "icon.png")))
         setupUi(self, os.path.join(getPluginPath(), "pyTSon", "scripts", "contactcleaner", "main.ui"))
         self.setWindowTitle("Contact Cleaner")
         
@@ -61,46 +61,69 @@ class Dialog(QDialog):
         self.ui_btn_check.clicked.connect(self.check)
         self.ui_btn_clean.clicked.connect(self.clean)
         
+        # Current time
         self.datenow_obj = datetime.datetime.now()
         
+        # Add options to combo
         self.ui_combo_time.addItem("1 month", 1)
         self.ui_combo_time.addItem("3 month", 3)
         self.ui_combo_time.addItem("6 month", 6)
         self.ui_combo_time.addItem("1 year", 12)
         self.ui_combo_time.addItem("2 years", 24)
-        self.ui_combo_time.addItem("3 years", 36)
-        self.ui_combo_time.addItem("more than 3 years", 99)
+        self.ui_combo_time.addItem("3 years amd more", 36)
         
-    def check(self):
+    def check(self):      
+        # keys array
         self.keys = []
+        
+        # Select all colums from contacts
         db = ts3client.Config()        
         q = db.query("SELECT * FROM contacts")
-        while q.next(): 
+        
+        # While rows
+        while q.next():             
+            # Split value string on new line
             val = q.value("value").split('\n')
+            
+            # For each line check if starts with LastSeen=
             for line in val:
                 if line.startswith('LastSeen='):
+                
+                    # Split line to get date
                     lastseen = line.split('LastSeen=')[1]
+                    
+                    # if empty then put key into keys and goto next row
                     if lastseen == "": 
                         self.keys.append(q.value("key"))
                         continue
+                        
+                    # create date obj from date string    
                     lastseen_obj = datetime.datetime.strptime(lastseen, "%Y-%m-%dT%H:%M:%S")          
  
+                    # difference between now and lastseen
                     difference = self.datenow_obj - lastseen_obj
 
+                    # if difference bigger than selected time then put key into ekys
                     if (self.ui_combo_time.currentData * 30) <= difference.days:
                         self.keys.append(q.value("key"))                      
         
+        # update selected users label
         self.ui_label_selected.setText("Selected users: "+str(len(self.keys)))
-        del db
         
-    def clean(self):
-        string = ""
-        for key in self.keys:
-            string = string+str(key)+", "
-        string = string[:-2]
-        db = ts3client.Config()        
-        q = db.query("DELETE FROM contacts WHERE key in ("+string+")")
-        del db
+    def clean(self):       
+        # if keys not empty
+        if self.keys:
+        
+            # convert keys array to string seperated by ,
+            seperator = ", "
+            key_string = seperator.join(self.keys)
+            
+            # delete data
+            db = ts3client.Config()        
+            q = db.query("DELETE FROM contacts WHERE key in ("+key_string+")")
+            
+            # crash teamspeak to make delete effective
+            qFatal("boom")
 
 
 
